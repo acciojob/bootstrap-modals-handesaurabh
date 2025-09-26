e48a2c588f9353427288848830e4a4d844daa98f
handesaurabh <handesaurabh2004@gmail.com>
2025-09-26 11:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 //your code here
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
